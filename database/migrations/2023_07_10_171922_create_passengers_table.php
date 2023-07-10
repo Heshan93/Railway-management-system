eefbf228d25dfd3299227615157235f155acc273
Heshan93 <heshanrajitha1@gmail.com>
2023-07-10 22:54:45 +0530

    create migration for user & passenger tables

---
+++ database/migrations/2023_07_10_171922_create_passengers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePassengersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passengers', function (Blueprint $table) {
+            $table->id('passenger_id');
+            $table->string('nic');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('tp_number');
+            $table->integer('dob');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passengers');
+    }
+}
