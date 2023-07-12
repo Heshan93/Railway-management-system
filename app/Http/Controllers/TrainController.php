75fa8e409eea719a24fb197611fcf7a1b5da6153
Heshan93 <heshanrajitha1@gmail.com>
2023-07-12 17:36:39 +0530

    add delay to other fun(not finish)

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ticket;
 use Illuminate\Support\Facades\DB;
+use DateTime;
+use DateInterval;
 
 class TrainController extends Controller
 {
@@ -22,24 +24,42 @@ class TrainController extends Controller
         return view('user_login');
     }
 
+/*
+    //add delay to arrival time
+
+    public function calDelay($arr_time, $delay, $amount)
+    {
+        $arrival = new DateTime($arr_time);
+        $delayTime = new DateTime('+' . $delay);
+    
+        $arrival->add($delayTime->diff($arrival));
+    
+        $updatedArrival = $arrival->format('Y-m-d H:i:s');
+    
+        $amount = floatval($amount);
+    
+        return $updatedArrival;
+    }
+    
+*/
+    
+    
+
     // get the train info to track
-    function trackTrain($id){
-
-          
-
-            if (session()->has('pName')) {
-
-                $data = DB::table('tickets')
+    function trackTrain($id) {
+        if (session()->has('pName')) {
+            $data = DB::table('tickets')
                 ->join('trains', 'tickets.train_id', '=', 'trains.train_id')
                 ->join('train_stations', 'tickets.st_no', '=', 'train_stations.st_no')
                 ->where('tickets.train_id', $id)
                 ->get();
-            
     
-                return view('train_info',['item'=>$data] );
+
     
-            }
-            return view('user_login');
+            return view('train_info', ['item' => $data]);
+        }
+        return view('user_login');
     }
+    
     
 }
