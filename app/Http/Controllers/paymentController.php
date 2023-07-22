1d237073058f657d4c35e6171b7b5f0587a9ac58
Heshan93 <heshanrajitha1@gmail.com>
2023-07-23 03:18:28 +0530

    Ticket Receipt email complete

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TickeReceipt;
+use Illuminate\Contracts\Session\Session;
 
 class paymentController extends Controller
 {
@@ -10,6 +13,13 @@ class paymentController extends Controller
 
     function makePayment(){
 
-        return "ssdfsd";
+         // $data for email template
+         $details  = [
+            'title' => "WRITEOFF-PRO Email Confirmation",
+            'name' => Session('AName'),
+          ];
+
+        Mail::to('fake@emai.com')->send(new TickeReceipt($details));  
+     
     }
 }
