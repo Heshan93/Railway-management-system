d1f77925ef01a4a149ec5a8f11e0f8ddb4b0a18b
Heshan93 <heshanrajitha1@gmail.com>
2023-07-25 10:06:51 +0530

    Bug Fix

--- app/Http/Controllers/profileConroller.php
+++ app/Http/Controllers/profileConroller.php
@@ -56,22 +56,24 @@ class profileConroller extends Controller
     }
 
     
-        function updatePassenger(Request $req){
+    function updatePassenger(Request $req){
 
-        
-        // Add validation 
-        $req->validate([
-            'firstName' => 'required',
-            'LastName' => 'required',
-            'email' => 'required|email',
-            'tp_number' => 'required',
-            'address' => 'required',
-            'InputPassword' => 'required|min:6',
-            'confirmInputPassword' => 'required|same:InputPassword',
+        if (session()->has('pName')) {
+    
+            
+            // Add validation 
+            $req->validate([
+                'firstName' => 'required',
+                'LastName' => 'required',
+                'email' => 'required|email',
+                'tp_number' => 'required',
+                'address' => 'required',
+                'InputPassword' => 'required|min:6',
+                'confirmInputPassword' => 'required|same:InputPassword',
 
-                ]);
+                    ]);
 
-            // Handle the registration process
+                // Handle the registration process
 
             
             
@@ -80,31 +82,35 @@ class profileConroller extends Controller
             
 
         
-        try {
-                // Update the new Passenger record
+            try {
+                    // Update the new Passenger record
 
-                $newUser = passenger::find(Session('passenger_id'));
+                    $newUser = passenger::find(Session('passenger_id'));
 
-                $newUser->passenger_id = $req->passenger_id;
-                $newUser->first_name = $req->firstName;
-                $newUser->last_name = $req->LastName;
-                $newUser->email = $req->email;
-                $newUser->password = Hash::make($req->InputPassword); // encrypt the password 
-                $newUser->tp_number = $req->tp_number;
-                $newUser->address =$req->address;
-                $rec  = $newUser->save();
+                    $newUser->passenger_id = $req->passenger_id;
+                    $newUser->first_name = $req->firstName;
+                    $newUser->last_name = $req->LastName;
+                    $newUser->email = $req->email;
+                    $newUser->password = Hash::make($req->InputPassword); // encrypt the password 
+                    $newUser->tp_number = $req->tp_number;
+                    $newUser->address =$req->address;
+                    $rec  = $newUser->save();
 
-                if ($rec) {
-                    return back()->with('success', 'You have successfully updated');
-                }
+                    if ($rec) {
+                        return back()->with('success', 'You have successfully updated');
+                    }
 
-            } catch (\Illuminate\Database\QueryException $e) {
+                } catch (\Illuminate\Database\QueryException $e) {
 
-                if ($e->getCode() === '23000') {
-                     // Other query exceptions
-                     return back()->with('fail', 'Something went wrong. Please try again.');
-                } 
-            }  
-        }
+                    if ($e->getCode() === '23000') {
+                        // Other query exceptions
+                        return back()->with('fail', 'Something went wrong. Please try again.');
+                    } 
+        
+                }  
+        } 
+        return redirect('admin'); 
+     }
+
 
 }
